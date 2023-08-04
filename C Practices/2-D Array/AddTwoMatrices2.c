8183847396c53fe62574a14e3c68bcc638421bf9
Shibasish <shibasishdas043@gmail.com>
2023-08-04 21:25:17 +0530

    all are ok

--- C Practices/2-D Array/AddTwoMatrices2.c
+++ C Practices/2-D Array/AddTwoMatrices2.c
@@ -1,2 +1,39 @@
 // Home Work = Write A Program To Add Two Matrices Without Using 3rd Array (Matrix) .
+#include<stdio.h>
+int main(){
+    int arr[2][2],brr[2][2];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+        
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            scanf("%d",&brr[i][j]);
+        }
+        
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            arr[i][j] = arr[i][j] + brr[i][j];
+        }
+        
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
--- C Practices/2-D Array/MaximumAndMinimumValue.c
+++ C Practices/2-D Array/MaximumAndMinimumValue.c
@@ -1,2 +1,53 @@
-// Home Work = Find Out The Maximum Element And Minimum Elementin A 2D Array .
+// Home Work = Find Out The Maximum Element And Minimum Element In A 2D Array .
 //You Have To Find The Index Of Maximum Element .
+#include<stdio.h>
+int main(){
+
+    int arr[3][3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+        
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    int max_element = arr[0][0];// 0th index Of Both Row And Column .
+    //Two variables maxElement and minElement are initialized with the value of arr[0][0] (the first element of the array) as a starting point.
+    int min_element = arr[0][0];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (arr[i][j] > max_element)
+            {
+                max_element = arr[i][j];
+            }
+            if (arr[i][j] < min_element)
+            {
+                min_element = arr[i][j];
+            }
+            
+        }
+        
+    }
+
+    //The program then iterates through all the elements of the array using nested loops. For each element, it checks if it is greater than the current maxElement. If it is, maxElement is updated with the new value. Similarly, it checks if the element is smaller than the current minElement, and if it is, minElement is updated with the new value.
+
+    printf("Maximum element: %d\n", max_element);
+    printf("Minimum element: %d\n", min_element);
+
+    return 0;
+}
