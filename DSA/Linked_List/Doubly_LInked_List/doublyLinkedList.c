69c76c5439379e21a77234660e648caae92f4ef4
Shibasish <shibasishdas043@gmail.com>
2024-12-30 23:33:48 +0530

    ok

---
+++ DSA/Linked_List/Doubly_LInked_List/doublyLinkedList.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *leftLink;
+    struct node *rightLink;
+};
+struct node *head;
+
+void insertAtHead(int value) {
+    struct node makeNode;
+    
+}
+
+int main(){
+
+return 0;
+}
