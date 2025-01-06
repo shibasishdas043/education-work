4e2af2fbac2210f54356fd02a164c6c2a4499caf
Shibasish <shibasishdas043@gmail.com>
2025-01-06 23:36:15 +0530

    ok

---
+++ DSA/Tree/Binary_Tree_1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *create_Node(struct node *root) {   
+    root = malloc(sizeof(struct node));
+    root
+
+}
+
+int main(){
+
+return 0;
+}
