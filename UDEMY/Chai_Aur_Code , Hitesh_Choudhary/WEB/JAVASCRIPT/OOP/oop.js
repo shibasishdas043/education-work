badd0ab853ae3e12b74bf48d99ef21327815867c
shibasishdas043 <shibasishdas043@gmail.com>
2025-01-24 20:49:11 +0530

    ok

--- UDEMY/Chai_Aur_Code , Hitesh_Choudhary/WEB/JAVASCRIPT/OOP/oop.js
+++ UDEMY/Chai_Aur_Code , Hitesh_Choudhary/WEB/JAVASCRIPT/OOP/oop.js
@@ -40,36 +40,69 @@
 // console.log(a.jumps())
 // console.log(a.Animal());
 
-class Animal{
-    constructor(name){
-        this.name = name;
-        console.log("Object Is Created .. ");
+// class Animal{
+//     constructor(name){
+//         this.name = name;
+//         console.log("Object Is Created .. ");
+//     }
+
+//     eat(){
+//         console.log("Khachi .. ");
+//     }
+
+//     jump(){
+//         console.log("lafgachi .. ");
+//     }
+// }
+
+// class Lion extends Animal{
+//     constructor(name){
+//         super(name)
+//         console.log("Object Is Created Its A Lion");
+//     }
+
+//     eat(){
+//         super.eat();
+//         console.log("Lafachi Road-e Te.. ");
+//     }
+// }
+
+// let a = new Animal("Lion");
+// console.log(a);
+
+// let l = new Lion("Chudir_Vai");
+// console.log(l);
+
+
+
+class User{
+    constructor(email, password){
+        this.email = email;
+        this.password = password;
+
     }
 
-    eat(){
-        return `Lafachi .. `;
+    get email(){
+        return this._email.toUpperCase();
     }
 
-    jump(){
-        return `Khachi .. `;
+    set email(value){
+        this._email = value.toUpperCase();
+        
+    }
+
+    get password(){
+        return `${this._password}shiba`;
+    }
+
+    set password(value){
+        this._password = value.toUpperCase()
     }
 }
 
-class Lion extends Animal{
-
-}
-
-let a = new Animal("Lion");
-console.log(a);
-
-let l = new Lion("Chudir_Vai");
-console.log(l);
-
-
-
-
-
-
+const shibasish = new User("shibasishdas043@gmail.com", "abc");
+console.log(shibasish.email);
+// console.log(shibasish.password);
 
 
 
