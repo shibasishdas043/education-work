a5f693acef0abf91ee72f46165551adebbeebdd3
shibasishdas043 <shibasishdas043@gmail.com>
2025-01-27 18:41:38 +0530

    ok

---
+++ UDEMY/Chai_Aur_Code , Hitesh_Choudhary/WEB/JAVASCRIPT/ADVANCED JAVASCRIPT/explore_1.js
@@ -0,0 +1,9 @@
+// let l = "Shibasish";
+// let count = 0;
+// for (let i = 0; i < l.length; i++){
+//     console.log(l[i]);
+
+//     count++;
+// }
+// console.log(count);
+// setTimeout(() => console.log("Hello . "), 5000);
