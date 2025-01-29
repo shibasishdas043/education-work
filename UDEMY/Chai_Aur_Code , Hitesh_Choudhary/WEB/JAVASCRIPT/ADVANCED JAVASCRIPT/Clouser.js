aa52c4fcb8665e7be1778792b1c3413bd50cec55
shibasishdas043 <shibasishdas043@gmail.com>
2025-01-29 20:19:26 +0530

    ok

---
+++ UDEMY/Chai_Aur_Code , Hitesh_Choudhary/WEB/JAVASCRIPT/ADVANCED JAVASCRIPT/Clouser.js
@@ -0,0 +1,50 @@
+//LEXICAL SCOPE
+function outer() {
+    let outerVar = 'I am from outer scope';
+
+    function inner() {
+        console.log(outerVar); // Accessing outerVar from the outer scope
+    }
+
+    inner();
+}
+
+// outer(); // Output: "I am from outer scope"
+
+//ENCAPSULATION
+function createCounter() {
+    let count = 0;
+
+    return function() {
+        count++;
+        return count;
+    };
+}
+
+const counter = createCounter();
+// console.log(counter()); // Output: 1
+// console.log(counter()); // Output: 2
+// console.log(counter()); // Output: 3
+
+
+function delayedGreeting(name) {
+    setTimeout(function() {
+        console.log(`Hello, ${name}!`);
+    }, 3000);
+}
+
+// delayedGreeting('Shibasish'); // Output after 1 second: "Hello, Alice!"
+
+
+
+function a(){
+    let name = "Shibasish";
+
+    function b(){
+        console.log(name);
+    }
+    return b();
+}
+
+let x = a();
+x();
--- UDEMY/Chai_Aur_Code , Hitesh_Choudhary/WEB/JAVASCRIPT/CONSTRUCTOR FUNCTIONS/Constructor_Functions.js
+++ UDEMY/Chai_Aur_Code , Hitesh_Choudhary/WEB/JAVASCRIPT/CONSTRUCTOR FUNCTIONS/Constructor_Functions.js
@@ -6,4 +6,22 @@ Animal.prototype.sound = function(){
     return `${this.species} Makes A Sound`;
 }
 let dog = new Animal("Dog")
-console.log(dog.sound())
+// console.log(dog.sound())
+
+function mapper(arr, fn){
+
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        let res = fn(arr[i], i);
+        result.push(res);
+    }
+    return result;
+}
+
+let arr = [1,2,3,4,5,6];
+let x = mapper(arr, function multiply(value, index){
+    console.log(value, value*value, index);
+    return value*value;
+});
+
+console.log(arr, x);
