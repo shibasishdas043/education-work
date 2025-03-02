7387a21d548afb2bf4c6427e3c7b2463677c2a72
shibasishdas043 <shibasishdas043@gmail.com>
2025-03-02 10:45:41 +0530

    ok

---
+++ UDEMY/Chai_Aur_Code , Hitesh_Choudhary/WEB/JAVASCRIPT PROJECTS/ECOMMERCE CART PAGE/script.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+	const products = [
+		{id:1, name:"product1", price:100},
+		{id:2, name:"product2", price:200},
+		{id:3, name:"product3", price:300},
+	];
+
+	const cart = [];
+	const productList = document.getElementById("product-list");
+
+
+
+
+
+
+
+
+
+
+
+})
+
