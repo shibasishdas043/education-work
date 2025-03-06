52b54e837c302b221cc2aa3f1a41b68ed9fcfe17
shibasishdas043 <shibasishdas043>
2025-03-06 12:11:16 +0530

    ok

--- UDEMY/Chai_Aur_Code , Hitesh_Choudhary/WEB/JAVASCRIPT/PLAYGROUND/PROMISES.js
+++ UDEMY/Chai_Aur_Code , Hitesh_Choudhary/WEB/JAVASCRIPT/PLAYGROUND/PROMISES.js
@@ -1,8 +1,24 @@
-function hello(value){
-    console.log("Hello");
-    value();
+// function hello(value){
+//     console.log("Hello");
+//     value();
+// }
+
+// hello(function bolo(){
+//     console.log("Bolo");
+// })
+
+function fetchData(url,fn){
+    console.log("Starting Download");
+
+    setTimeout(function download(){
+        console.log("Download Finished");
+        
+        let data = "Dummy Data";
+        fn(data);
+
+    },3000);
 }
 
-hello(function bolo(){
-    console.log("Bolo");
-})
+let x = fetchData("www.google.com", function downloded(response){
+    console.log("Downloaded : ",response);
+});
