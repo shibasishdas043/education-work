46690464ce5362154a38874f9ae3ef0a49b4894c
shibasishdas043 <shibasishdas043@gmail.com>
2025-03-13 12:17:30 +0530

    ok

---
+++ UDEMY/CHAI_AUR_CODE _HITESH_CHOUDHARY/WEB/JAVASCRIPT/PLAYGROUND/ASYNC-AWAIT.js
@@ -0,0 +1,22 @@
+function fetchUserData() {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            resolve({name:"Chaicode", url:"https://chaicode.com"});
+        },3000)
+
+    })
+
+}
+
+async function getUserData() {
+
+    try {
+        console.log("fetching");
+        const userdata = await fetchUserData();
+        console.log("User Data: ", userdata);
+    } catch (error) {
+        console.log("Error Fetching !", error);
+    }
+    
+}
