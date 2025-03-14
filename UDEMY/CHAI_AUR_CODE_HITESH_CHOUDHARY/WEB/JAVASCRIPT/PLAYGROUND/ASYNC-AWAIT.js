8af195ce40d8175f9bc276887c76d3999042f30e
shibasishdas043 <shibasishdas043@gmail.com>
2025-03-14 10:16:02 +0530

    ok

--- UDEMY/CHAI_AUR_CODE_HITESH_CHOUDHARY/WEB/JAVASCRIPT/PLAYGROUND/ASYNC-AWAIT.js
+++ UDEMY/CHAI_AUR_CODE_HITESH_CHOUDHARY/WEB/JAVASCRIPT/PLAYGROUND/ASYNC-AWAIT.js
@@ -1,24 +1,64 @@
-function fetchUserData() {
-    return new Promise((resolve, reject) => {
+// function fetchUserData() {
+//     return new Promise((resolve, reject) => {
 
-        setTimeout(() => {
-            resolve({name:"Chaicode", url:"https://chaicode.com"});
-        },3000)
+//         setTimeout(() => {
+//             resolve({name:"Chaicode", url:"https://chaicode.com"});
+//         },3000)
 
-    })
+//     })
 
-}
+// }
 
-async function getUserData() {
+// async function getUserData() {
 
-    try {
-        console.log("fetching");
-        const userdata = await fetchUserData();
-        console.log("User Data: ", userdata);
-    } catch (error) {
-        console.log("Error Fetching !", error);
-    }
+//     try {
+//         console.log("fetching");
+//         const userdata = await fetchUserData();
+//         console.log("User Data: ", userdata);
+//     } catch (error) {
+//         console.log("Error Fetching !", error);
+//     }
     
+// }
+
+// getUserData();
+
+
+
+function fetchPostData(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Commented");
+        },2000);
+    })
 }
 
-getUserData();
+function fetchCommentData(){
+    return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve("Fetched Successful");
+        },4000);
+    })
+}
+
+async function getBlogData() {
+    try {
+        console.log("Fetching");
+        // const x = await fetchPostData();
+        // const y = await fetchCommentData();
+        // console.log(x);
+        // console.log(y);
+
+        const [postData, commentData] = await Promise.all([fetchPostData(), fetchCommentData(),]);
+        console.log(postData);
+        console.log(commentData);
+        
+        console.log("Fetch Complete !");
+    } catch (error) {
+        console.log("error !", error);
+    }
+}
+
+getBlogData();
+
+
