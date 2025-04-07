0a35a5faea6aa49b7f77cfe06f9543b29bc2ca45
shibasishdas043 <shibasishdas043@gmail.com>
2025-04-07 23:46:08 +0530

    ok

---
+++ REACT/DEMO 1/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Greeting from "./Greeting";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/DEMO 1/my-react-app/src/Greeting.jsx
@@ -0,0 +1,10 @@
+function Greeting() {
+  return (
+    <div>
+      <h2>Welcome to React Components</h2>
+      <p>This is a reusable component.</p>
+    </div>
+  );
+}
+
+export default Greeting;
