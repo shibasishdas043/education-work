a3762addffe0e876fac2bbaf8bd83fda77a77954
shibasishdas043 <shibasishdas043@gmail.com>
2025-04-07 11:35:25 +0530

    ok

---
+++ NODE/todo.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const filePath = "./tasks.json";
+
+
+function loadTask(){
+    try {
+        const dataBuffer = fs.readFileSync(filePath)
+        const dataJSON = dataBuffer.toString();
+
+        return JSON.parse(dataJSON);
+    } catch (error) {
+        return [];
+    }
+}
+
+function saveTasks(tasks){
+    const dataJSON = JSON.stringify(tasks);
+    fs.writeFileSync(filePath, dataJSON);
+}
+
+function addTask(argument){
+    const tasks = loadTask();
+    tasks.push({tasks});
+    saveTasks(tasks);
+    console.log("Tasks Are Added !", tasks);
+}
+
+
+const command = process.argv[2];
+const argument = process.argv[3];
+
+
+
+if(command === "add"){
+    addTask(argument)
+}else if(command === "list"){
+    listTask()
+}else if(command === "remove"){
+    removeTask(parseInt(argument))
+}else{
+    console.log("command not found");
+}
