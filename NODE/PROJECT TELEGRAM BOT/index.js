da1358f864dbc8442b9c4a301409f050aed8eb9c
shibasishdas043 <shibasishdas043@gmail.com>
2025-04-09 15:14:53 +0530

    ok

--- NODE/PROJECT TELEGRAM BOT/index.js
+++ NODE/PROJECT TELEGRAM BOT/index.js
@@ -1,14 +1,19 @@
+
 const { Telegraf } = require("telegraf");
 const { message } = require("telegraf/filters");
-
 
 BOT_TOKEN = "7968727999:AAELEaZT-bXe8Dg7AQPq_sfvQZBfFcunv2s";
 const bot = new Telegraf(BOT_TOKEN);
 
+bot.start((ctx) =>
+  ctx.reply(
+    "Hello Amar Gandu Choda Pabitra Das Ami Apnar Ki Sahajyo Korte Pari"
+  )
+);
 
-bot.start((ctx) => ctx.reply("Hello Amar Gandu Choda Pabitra Das Ami Apnar Ki Sahajyo Korte Pari"));
-
-bot.hears("Hello", (ctx) => ctx.reply("Hello Mane Banchod , Ki Bolchis , Amar Baper Nam Aajay Das"))
+bot.hears("Hello", (ctx) =>
+  ctx.reply("Hello Mane Banchod , Ki Bolchis , Amar Baper Nam Aajay Das")
+);
 
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 
@@ -17,5 +22,8 @@ bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
 bot.hears("tooi ke", (ctx) => ctx.reply("Ami Banchod Ajay er beta"));
 bot.hears("toi ke", (ctx) => ctx.reply("Ami Banchod Ajay er beta"));
 
+
+
+bot.command("ki bolis", (ctx) => ctx.reply(response));
 
 bot.launch();
