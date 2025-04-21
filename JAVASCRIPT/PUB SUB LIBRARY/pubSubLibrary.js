2a62be975cbfd20170c3188ccc8742ab0dcc207d
shibasishdas043 <shibasishdas043@gmail.com>
2025-04-21 13:15:30 +0530

    ok

---
+++ JAVASCRIPT/PUB SUB LIBRARY/pubSubLibrary.js
@@ -0,0 +1,51 @@
+class PubSub {
+  constructor() {
+    this.subscribers = {};
+  }
+
+  /**
+   *
+   * @param event --> its is a string denoting unique event fired
+   * @param callback --> for a subcriber what method should be executed when an event is fired
+   */
+  subscribe(event, callback) {
+    if (!this.subscribers[event]) {
+      //curenntly no subscriber event was registered for the event
+      this.subscribers[event] = [];
+    }
+    this.subscribers[event].push(callback);
+  }
+
+  /**
+   *
+   * @param  event --> its is a string denoting unique event fired
+   * @param  data -->for the given event what data should be passed along with publishing the event
+   */
+  publish(event, data) {
+    if (!this.subscribers[event]) return;
+
+    this.subscribers[event].forEach(callback => callback(data));
+        
+  }
+}
+
+
+const pb = new PubSub();
+
+
+pb.subscribe("airforce", (data) => console.log("Subscrber 1", data));
+
+pb.subscribe("airforce", (data) => console.log("Subscrber 2", data));
+
+pb.subscribe("airforce", (data) => console.log("Subscrber 3", data));
+
+
+
+pb.subscribe("new balance", (data) => console.log("Subscrber 1", data));
+
+
+
+pb.publish("airforce", {shoename: "jordan airforce"});
+
+pb.publish("new balance", { shoename: "something" });
+
