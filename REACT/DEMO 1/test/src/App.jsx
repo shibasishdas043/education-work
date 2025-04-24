49d0656d3126c8be8d2652a5d2c1b7b7e41f99d4
shibasishdas043 <shibasishdas043@gmail.com>
2025-04-24 11:59:12 +0530

    ok

---
+++ REACT/DEMO 1/test/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+      <div className="main-div">
+
+        <div id="child-div-1">
+          <h1>Hello Its A Good Day</h1>
+        </div>
+
+        <div id="child-div-2">
+          <p>Today Is Going To Held A Match With RR Vs Rcb</p>
+        </div>
+
+      </div>
+  );
+}
+
+export default App
