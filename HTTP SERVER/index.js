ad5a3bc219c4157dd89c747f2920c08234af8a0f
Shibasish Das <107395100+shibasishdas043@users.noreply.github.com>
2026-04-08 19:49:14 +0530

    ok

---
+++ HTTP SERVER/index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+
+const PORT = 3000;
+
+//using the createServer fumction we can actually create a basic http server
+
+//this function returns a server object, and takes a callback as an argument
+
+//this function created a server object but didn't start the server
+const server = http.createServer(function listener(request, response) {
+  //request ---> using request we will be see the details of incoming http request --> Object
+
+  //response--->we will be able to configure what response we nees to send for an incoming http request --> Object
+
+  //this call back is a kind of listener function
+
+  //that is going to collect every http request that we will make to our server
+
+  if (request.url=='/home') {
+    //if we make a request on /home this block will be executed
+    console.log(request.method);
+    response.end('Welcome To Home');
+  }
+
+  console.log("Request Recieved");
+});
+
+server.listen(PORT, function exec() {
+  //once we succesfully boot the server on the given PORT, this callback is executed
+
+  console.log("Server Is Up And Running On PORT : ", PORT);
+});
---
+++ MONGODB CRASH COURSE/01_setup.mongodb.js
@@ -0,0 +1,7 @@
+use("firstDB");
+
+db.getSelection("firstCollection").insertOne({
+  _id: "69c2acd8be67dd3914sdfsd06a",
+  name: "Rupam Sardar",
+  ph: 2000000000,
+});
